ddb8e67f060ce6c99b91ca2e6662d09639a46f7d
erkam.karaca <erkamkaracaa@gmail.com>
2024-07-18 19:24:49 +0300

    Dashboard content component refactored.

--- frontend/src/components/DashboardContent/DashboardContent.tsx
+++ frontend/src/components/DashboardContent/DashboardContent.tsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import styles from "./DashboardContent.module.css";
 import ListingCard from "@/components/ListingCard/ListingCard";
 import PackageCard from "@/components/PackageCard/PackageCard";
+import FilterSection from "@/components/FilterSection/FilterSection";
 
 type DashboardContentProps = {
   listings: any[];
@@ -62,77 +63,16 @@ const DashboardContent: React.FC<DashboardContentProps> = ({
     setFilteredListings(updatedListings);
   }, [filter, typeFilter, priceFilter, dateFilter, listings]);
 
-  const handleFilterChange = (status: string) => {
-    setFilter(status);
-  };
-
-  const handleTypeFilterChange = (type: string) => {
-    setTypeFilter(type);
-  };
-
-  const handlePriceFilterChange = (priceOrder: string) => {
-    setPriceFilter(priceOrder);
-  };
-
-  const handleDateFilterChange = (dateOrder: string) => {
-    setDateFilter(dateOrder);
-  };
-
   return (
     <div className={styles.dashboardContentContainer}>
       <h2>Portfolio</h2>
-      <div className={styles.filterContainer}>
-        <div className={styles.dropdownFilterSection}>
-          <span className={styles.filterText}>
-            Type:
-            <select
-              className={styles.filterDropdown}
-              onChange={(e) => handleTypeFilterChange(e.target.value)}
-            >
-              <option value="ALL">All Types</option>
-              <option value="FLAT">Flat</option>
-              <option value="STORE">Store</option>
-              <option value="HOUSE">House</option>
-              <option value="LAND">Land</option>
-            </select>
-          </span>
-          <span className={styles.filterText}>
-            Price:
-            <select
-              className={styles.filterDropdown}
-              onChange={(e) => handlePriceFilterChange(e.target.value)}
-            >
-              <option value="ALL">All Prices</option>
-              <option value="LOW_TO_HIGH">Low to High</option>
-              <option value="HIGH_TO_LOW">High to Low</option>
-            </select>
-          </span>
-          <span className={styles.filterText}>
-            Date:
-            <select
-              className={styles.filterDropdown}
-              onChange={(e) => handleDateFilterChange(e.target.value)}
-            >
-              <option value="ALL">All Dates</option>
-              <option value="NEW_TO_OLD">New to Old</option>
-              <option value="OLD_TO_NEW">Old to New</option>
-            </select>
-          </span>
-        </div>
-        <div className={styles.statusFilterSection}>
-          {["ALL", "ACTIVE", "PASSIVE"].map((status) => (
-            <div
-              key={status}
-              className={`${styles.filterOption} ${
-                filter === status ? styles.selected : ""
-              }`}
-              onClick={() => handleFilterChange(status)}
-            >
-              {status}
-            </div>
-          ))}
-        </div>
-      </div>
+      <FilterSection
+        handleTypeFilterChange={setTypeFilter}
+        handlePriceFilterChange={setPriceFilter}
+        handleDateFilterChange={setDateFilter}
+        handleStatusFilterChange={setFilter}
+        filter={filter}
+      />
       {filteredListings ? (
         <div className={styles.gridContainer}>
           {filteredListings.map((listing: any) => (
---
+++ frontend/src/components/FilterSection/FilterSection.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import React from "react";
+import styles from "./FilterSection.module.css";
+
+type FilterSectionProps = {
+  handleTypeFilterChange: (type: string) => void;
+  handlePriceFilterChange: (priceOrder: string) => void;
+  handleDateFilterChange: (dateOrder: string) => void;
+  handleStatusFilterChange: (status: string) => void;
+  filter: string;
+};
+
+const FilterSection: React.FC<FilterSectionProps> = ({
+  handleTypeFilterChange,
+  handlePriceFilterChange,
+  handleDateFilterChange,
+  handleStatusFilterChange,
+  filter,
+}) => {
+  return (
+    <div className={styles.filterContainer}>
+      <div className={styles.dropdownFilterSection}>
+        <span className={styles.filterText}>
+          Type:
+          <select
+            className={styles.filterDropdown}
+            onChange={(e) => handleTypeFilterChange(e.target.value)}
+          >
+            <option value="ALL">All Types</option>
+            <option value="FLAT">Flat</option>
+            <option value="STORE">Store</option>
+            <option value="HOUSE">House</option>
+            <option value="LAND">Land</option>
+          </select>
+        </span>
+        <span className={styles.filterText}>
+          Price:
+          <select
+            className={styles.filterDropdown}
+            onChange={(e) => handlePriceFilterChange(e.target.value)}
+          >
+            <option value="ALL">All Prices</option>
+            <option value="LOW_TO_HIGH">Low to High</option>
+            <option value="HIGH_TO_LOW">High to Low</option>
+          </select>
+        </span>
+        <span className={styles.filterText}>
+          Date:
+          <select
+            className={styles.filterDropdown}
+            onChange={(e) => handleDateFilterChange(e.target.value)}
+          >
+            <option value="ALL">All Dates</option>
+            <option value="NEW_TO_OLD">New to Old</option>
+            <option value="OLD_TO_NEW">Old to New</option>
+          </select>
+        </span>
+      </div>
+      <div className={styles.statusFilterSection}>
+        {["ALL", "ACTIVE", "PASSIVE"].map((status) => (
+          <div
+            key={status}
+            className={`${styles.filterOption} ${
+              filter === status ? styles.selected : ""
+            }`}
+            onClick={() => handleStatusFilterChange(status)}
+          >
+            {status}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FilterSection;
